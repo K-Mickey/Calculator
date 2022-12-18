f246d608a086c8f434c92cc4d4596c0edcb79b49
K-Mickey <kokmickey@yandex.ru>
2022-12-18 22:01:55 +0400

    settigs changes

--- Calculate/settings.py
+++ Calculate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#(=$rgnxdvv3-ykclje85^@jm3-c2^hk#cof%c9=_dcy2yp77&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
